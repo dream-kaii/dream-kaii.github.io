5ff165f821ae2e75a966f0a420183ce227e28154
dream-kaii <2157497451@qq.com>
2026-03-31 10:51:54 +0800

    Site updated: 2026-03-31 10:51:51

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+// 打字机效果
+var typed = new Typed('.subtitle', {
+    strings: ['欢迎来到我的博客', '记录生活与技术', '分享学习心得'],
+    typeSpeed: 100,
+    backSpeed: 50,
+    startDelay: 500,
+    backDelay: 1500,
+    loop: true,
+    showCursor: true,
+    cursorChar: '|'
+});
